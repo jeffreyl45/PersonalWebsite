7eb25789d97c2a272c51a3dbc50c51d08df2c78e
jeffreyl45 <jefflu288@gmail.com>
2023-12-31 21:48:39 -0500

    linked contact me to google sheets

--- index.js
+++ index.js
@@ -16,3 +16,24 @@ function opentab(tabName) {
 
 function expandProject(){
 }
+
+function submitForm(){
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbzlOHAY1jkrFBSCnA-SWcYp-sk3akDfyIxK6ol6yX1P6tiwgr_bfnUDVYmnxOhHTaS2/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Your Message Was Sent Successfully!"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        }, 5000)
+        form.reset();
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
+}
