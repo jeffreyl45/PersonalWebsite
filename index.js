32ec4528ae006cde7f18b7efbdf6fea6f0a75ad7
jeffreyl45 <jefflu288@gmail.com>
2023-12-31 19:27:56 -0500

    added comments to index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var tabLinks = document.getElementsByClassName("tab-links");
 var tabContents = document.getElementsByClassName("tab-contents");
 
+
+// control the tab changes under the About Me section
 function opentab(tabName) {
     for(tabLink of tabLinks) {
         tabLink.classList.remove("active-link");
