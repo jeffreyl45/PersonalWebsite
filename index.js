d25edbf567d2980c45f0b449035564cab3bb9e9a
jeffreyl45 <jefflu288@gmail.com>
2024-01-01 20:36:57 -0500

    added media query

--- index.js
+++ index.js
@@ -76,3 +76,12 @@ function submitForm(){
 function statusUpdate(msg) {
     msg.innerHTML = "Please wait"
 }
+
+const sideMenu = document.getElementById("sideMenu");
+function openMenu() {
+    sideMenu.style.right = "0";
+}
+
+function closeMenu() {
+    sideMenu.style.right = "-200px";
+}
